f2c2c21f87e551dfa7c7bdff34ea27b97cc80d66
yeswanthvuddagiri <yeswanthyeswanth20@gmail.com>
2025-05-27 12:25:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,45 @@
 // service-worker.js
 
-self.addEventListener('install', function (event) {
-  console.log('Service Worker installed.');
+const CACHE_NAME = 'chatbot-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  // Add more static files if needed (e.g., /style.css, /app.js)
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Service Worker: Caching files');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activated');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker: Removing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });

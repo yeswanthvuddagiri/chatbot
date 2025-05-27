ce7b21ea3823a582e6b6fecccf36ed85cd51e8fb
yeswanthvuddagiri <yeswanthyeswanth20@gmail.com>
2025-05-27 12:19:15 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+// service-worker.js
+
+self.addEventListener('install', function (event) {
+  console.log('Service Worker installed.');
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fetch(event.request));
+});

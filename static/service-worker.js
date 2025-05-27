f3b5646c6bbe73763eaabc51c4d99075601fafa8
yeswanthvuddagiri <yeswanthyeswanth20@gmail.com>
2025-05-27 12:51:21 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,30 @@
 // service-worker.js
 
-self.addEventListener('install', function (event) {
-  console.log('Service Worker installed.');
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open('weather-chatbot-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheResponse) => {
+      return cacheResponse || fetch(event.request);
+    })
+  );
 });

a581d65287589152732bdff1a6022adad0dd015c
yeswanthvuddagiri <yeswanthyeswanth20@gmail.com>
2025-05-28 12:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-pro")  # OK
+model = genai.GenerativeModel("models/gemini-1.5-pro")
 
 def get_weather(city):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric'
